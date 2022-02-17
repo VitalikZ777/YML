fce95dcb24d953afd9b0402552fd51f314fbbbf0
VitalikZ777 <48833594+VitalikZ777@users.noreply.github.com>
2022-02-17 17:12:56 +0200

    Add files via upload

---
+++ yml_auto_prom/Attribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yml_auto_prom
+{
+    class Attribute
+    {
+
+
+        public string nameoption { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+
+    }
+}
