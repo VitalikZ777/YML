bb682d56a250da6dfbe31144e693f06e27ec9f32
VitalikZ777 <48833594+VitalikZ777@users.noreply.github.com>
2022-02-17 17:18:04 +0200

    Add files via upload

---
+++ YML_Forms/Picture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YML_Forms
+{
+    class Picture
+    {
+
+        public string picture_Id { get; set; }
+        public string picture_url { get; set; }
+        public string picture_type { get; set; }
+
+
+    }
+}
